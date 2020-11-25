f0abfa85d94bfc8ab775518cdfab1cf16f769b45
Vlad Babii <vlad@babii.ro>
2020-11-25 09:36:45 +0000

    auto

--- scripts/delete_sd_card_files_older_than_a_week.sh
+++ scripts/delete_sd_card_files_older_than_a_week.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+## put this in cron with
+## */30 * * * * /bin/bash /home/pi/klipper_config/scripts/delete_sd_card_files_older_than_a_week.sh 
+
 find /home/pi/sdcard/ -mtime +7 | xargs rm -f
