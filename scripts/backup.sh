ee0dee3116634e6127a8bd72ebbdeaa2ace32213
Vlad Babii <vladbabii@users.noreply.github.com>
2020-12-04 00:49:28 +0200

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -12,21 +12,24 @@ git remote show origin >> /home/pi/klipper_config/docs/version_kiauh.md
 echo "\`\`\`" >> /home/pi/klipper_config/docs/version_kiauh.md
 
 cd /home/pi/klipper/
-git config --get remote.origin.url > /home/pi/klipper_config/docs/version_klipper.md
+echo "\`\`\`" > /home/pi/klipper_config/docs/version_klipper.md
+git config --get remote.origin.url >> /home/pi/klipper_config/docs/version_klipper.md
 git describe --tags >> /home/pi/klipper_config/docs/version_klipper.md
 git rev-parse --short HEAD >> /home/pi/klipper_config/docs/version_klipper.md
 git remote show origin >> /home/pi/klipper_config/docs/version_klipper.md
+echo "\`\`\`" >> /home/pi/klipper_config/docs/version_klipper.md
 
 #cd /home/pi/mainsail/
 # ??
 #git describe --tags | tee /home/pi/klipper_config/docs/version_mainsail.md
 
 cd /home/pi/moonraker/
-git config --get remote.origin.url > /home/pi/klipper_config/docs/version_moonraker.md
+echo "\`\`\`" > /home/pi/klipper_config/docs/version_moonraker.md
+git config --get remote.origin.url >> /home/pi/klipper_config/docs/version_moonraker.md
 git describe --tags >> /home/pi/klipper_config/docs/version_moonraker.md
 git rev-parse --short HEAD >> /home/pi/klipper_config/docs/version_moonraker.md
 git remote show origin >> /home/pi/klipper_config/docs/version_moonraker.md
-
+echo "\`\`\`" >> /home/pi/klipper_config/docs/version_moonraker.md
 
 cd /home/pi/klipper_config
 git pull
